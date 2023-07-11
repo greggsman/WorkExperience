006e7da1a966bea17c11787f9b6bd33959e3bedb
greggsman <elliotdg10@icloud.com>
2023-07-11 16:28:44 +0100

    changed the name of localfile to results file made a test python script to receive a prediction from the ada bot to predict description from the prompt writes the prediction alongside the actual answer

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
 import os
 import openai
+import random
 
 openai.api_key = "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"
-
-response = openai.Completion.create(
-  model="text-ada-001",
-  prompt="Write a headline for an article about pythagoras",
-  temperature=0,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
+runNext = "Y"
+while(runNext == "Y"):
+    response = openai.Completion.create(
+        model="text-ada-001",
+        prompt="Write a headline for an article about pythagoras",
+        temperature=0,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    with open('training_prepared.jsonl', 'r', encoding="UTF-8") as json_file:
+        json_list = list(json_file)
+        currentTest = json_list[random.randint(0, len(json_list) - 1)]
+        json_file.close()
+    with open('results.txt', 'a', encoding="UTF 8") as resultsFile:
+        resultsFile.write(f"Actual completion: {currentTest['completion']}, Prediction: {response}")
+    runNext = input("Run the program again?")
