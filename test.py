84fdda4cbbbbf35c9adbd46c55c79b5749ae577d
greggsman <elliotdg10@icloud.com>
2023-07-11 17:22:39 +0100

    this works for some reason idk why, i'll keep it here just in case

--- test.py
+++ test.py
@@ -1,23 +1,28 @@
 import os
 import openai
 import random
+import json
 
 openai.api_key = "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"
 runNext = "Y"
+
 while(runNext == "Y"):
+    currentTest = {}
+    with open('testing_prepared.jsonl', 'r', encoding="UTF-8") as json_file:
+        json_list = list(json_file)
+        currentTest = json.loads(json_list[random.randint(0, len(json_list) - 1)])
+        json_file.close()
     response = openai.Completion.create(
-        model="text-ada-001",
-        prompt="Write a headline for an article about pythagoras",
-        temperature=0,
+        model="ada:ft-personal-2023-07-11-15-31-15",
+        prompt=currentTest["prompt"],
+        temperature=0.7,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
     )
-    with open('training_prepared.jsonl', 'r', encoding="UTF-8") as json_file:
-        json_list = list(json_file)
-        currentTest = json_list[random.randint(0, len(json_list) - 1)]
-        json_file.close()
-    with open('results.txt', 'a', encoding="UTF 8") as resultsFile:
-        resultsFile.write(f"Actual completion: {currentTest['completion']}, Prediction: {response}")
+    with open('results.txt', 'a', encoding="UTF-8") as resultsFile:
+        resultsFile.write(f"Prompt:{currentTest['prompt']} | Actual completion: {currentTest['completion']} | Prediction: {response['choices'][0]['text']}\n")
     runNext = input("Run the program again?")
+    if(runNext != "Y"):
+        break
