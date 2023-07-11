92a09cc0fd7d3274c1a47777dd6006fa6977cc86
greggsman <elliotdg10@icloud.com>
2023-07-11 15:27:10 +0100

    changed the prompt on the test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import os
 import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 
 response = openai.Completion.create(
   model="text-ada-001",
-  prompt="",
+  prompt="Write a headline for an article about pythagoras",
   temperature=0,
   max_tokens=256,
   top_p=1,
