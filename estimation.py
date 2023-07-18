e07167e5fc90ef019a086c121cdfd744a63bdd8a
greggsman <elliotdg10@icloud.com>
2023-07-18 12:12:19 +0100

    Changed a token to a group of 3 characters To make the estimation closer to the actual value

--- estimation.py
+++ estimation.py
@@ -2,27 +2,23 @@ import json
 
 with open('training_prepared.jsonl', 'r', encoding="UTF-8") as json_file:
     json_list = list(json_file.readlines())
-
 spacecount = 0
+charCount = 0
 previous = {}
+tokenLengthAverage = 3
 # counts spaces to estimate number of tokens
 for i in range(len(json_list)):
     result = json.loads(json_list[i])
     currentPrompt = result["prompt"]
     currentCompletion = result["completion"]
-    if(currentPrompt == previous): # disregarding duplicates
-        continue
-    # count the spaces 
-    for c in str(currentPrompt):
-        if c == ' ':
-            spacecount += 1
-    for c in str(currentCompletion):
-        if c == ' ':
-            spacecount += 1
-    previous = currentPrompt
+    for c in range(len(currentPrompt)):
+        if c % tokenLengthAverage == 0:
+            charCount += 1
+    for c in range(len(currentCompletion)):
+        if c % tokenLengthAverage == 0:
+            charCount += 1
 
-print(spacecount)
 def estimate(price):
-    return spacecount * price/1000
+    return (charCount/1000) * price
 
-print(f"Ada: ${estimate(0.0004)} \n Babbage: ${estimate(0.0006)} \n Curie: ${estimate(0.003)} \n Davinci: ${estimate(0.03)}" )
+print(f"Ada: ${estimate(0.0004)}\nBabbage: ${estimate(0.0006)}\nCurie: ${estimate(0.003)}\nDavinci: ${estimate(0.03)}\nTurbo 3.5: {estimate(0.0015)}")
