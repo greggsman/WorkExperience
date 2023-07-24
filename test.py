82afbe95d73b4c752dcb3424bb4ceab0b298e810
greggsman <128397291+greggsman@users.noreply.github.com>
2023-07-24 14:43:11 +0100

    Update test.py
    Got rid of some stuff to resolve a conflict

--- test.py
+++ test.py
@@ -41,16 +41,3 @@ trainedBabbage = "babbage:ft-personal-2023-07-13-12-23-04"
 gptTurbo = "gpt-3.5-turbo-0613"
 gptTurboPreamble = "Write a product line description for the following product name. The product description should be suitable to display the product on an online shopping website\n\nFor Example: \nProduct name: \"\"\"Awkward Styles Sugar Skull Shirts for Men Jolly Roger Skull and Crossbones Men's Tee Shirt Tops Day of Dead Tshirts Pirate Flag Shirts Skull T-shirts Dia de Los Muertos T Shirts for Men\"\"\"\n\nProduct Description: \"\"\"Are you planning a pirate themed party? Then this Awkward Styles Jolly Roger Skull &amp; Crossbones Graphic T-shirt Tops will be the perfect fit! This fun Jolly Roger Skull &amp; Crossbones Graphic T-shirt Tops wil also make a great gift for halloween, day of the dead, cinco de mayo, pirate lovers, birthdays and other special occasions or for everyday wear!| Aaaargh....Are you planning a pirate themed party? Then this Awkward Styles Jolly Roger Skull Graphic T-shirt Tops is the perfect fit! All Hands Hoay! Still searching for a Halloween costume? Why not go with this cool Jolly Roger Skull Graphic T-shirt Tops and be a pirate for the night. Perfect gift for Day of the Dead, Cinco de Mayo, Halloween, Birthdays or other special occasions! This Jolly Roger Skull Graphic T-shirt Tops is perfect for Halloween, Day of the Dead, Cinco de Mayo but it is also perfect for everyday wear! Celebrate the colorful mexican tradition in this cool Jolly Roger Skull Graphic T-shirt Tops 100% cotton Printed in the USA Available sizes: S-5XL Machine washable Available in a wide variety of sizes and colors\"\"\"\n##\nProduct Name: "
 
-runNext = "Y"
-while(runNext == "Y"):
-    currentTest = {}
-    with open('testing_prepared.jsonl', 'r', encoding="UTF-8") as json_file:
-        json_list = list(json_file)
-        currentTest = json.loads(json_list[random.randint(0, len(json_list) - 1)])
-        json_file.close()
-    print(currentTest["prompt"])
-    RunTest(trainedAda, currentTest)
-    RunTest(trainedBabbage, currentTest)
-    RunTest(gptTurbo, currentTest, gptTurboPreamble)
-    runNext = input("Run the program again?")
-    
